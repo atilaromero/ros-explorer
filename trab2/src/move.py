d57b3316ff7c731a633848e56a242293d42a1202
Atila Romero <atilaromero@gmail.com>
2018-11-10 12:45:14 -0200

    window

--- trab2/src/move.py
+++ trab2/src/move.py
@@ -4,55 +4,86 @@ import sys
 import rospy
 import numpy as np
 from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
-import tf
+import cv2
+import threading
+import time
+from matplotlib.colors import Normalize
+import matplotlib.cm as cm
 
 def normRad(rad):
     return (rad + np.pi) % (2*np.pi) - np.pi
 
-class Trab1():
-    def __init__(self, goalX, goalY):
-        self.goalX = float(goalX)
-        self.goalY = float(goalY)
+def ranges2cart(ranges, range_min, range_max, angle_min, angle_increment):
+    imgsize = 200
+    rangestep = 10.0/imgsize
+    ranges = np.array(ranges)
+    r, phi = np.mgrid[0:range_max:rangestep,-np.pi:np.pi:angle_increment]
+    phimin_index = int((angle_min+np.pi)/angle_increment)
+    ranges2d = np.zeros(r.shape)
+    ranges2d[:,phimin_index:phimin_index+ranges.shape[0]]=ranges
+    ranges2d[np.isnan(ranges2d)] = range_max+2
+    v = np.zeros(r.shape)
+    v[r>ranges2d] = 0
+    v[r<ranges2d] = -1
+    v[r<range_min] = 0
+    v[(ranges2d!=0) & (np.abs(r-ranges2d)<0.1)] =1
+    dst = cv2.linearPolar(v.T, (imgsize,imgsize),imgsize,cv2.WARP_FILL_OUTLIERS | cv2.WARP_INVERSE_MAP)
+    dst2 = np.zeros((imgsize*2,imgsize*2))
+    dst2[:,imgsize:] = dst[:imgsize*2,:][:,::-1] 
+    return dst2
+
+def showImage(obj):
+    norm = Normalize(vmin=-1, vmax=1)
+    cv2.namedWindow('showImage')
+    while(not rospy.is_shutdown()):
+        if not hasattr(obj, "scan"):
+            time.sleep(0.1)
+            continue
+        msg = obj.scan
+        image = ranges2cart(msg.ranges, msg.range_min, msg.range_max, msg.angle_min, msg.angle_increment)
+        cv2.imshow("showImage", cm.hot(norm(image)))
+        cv2.waitKey(1)
+    cv2.destroyWindow('showImage')
+        
+
+class Trab2():
+    def __init__(self):
         self.linearResolution = 0.2
-        rospy.init_node('Trab1', anonymous=False)
+        self.pos = lambda:None
+        self.pos.x = 0
+        self.pos.y = 0
+        self.pos.rot = 0
+        rospy.init_node('Trab2', anonymous=False)
         rospy.on_shutdown(self.shutdown)
-        def getPose(msg):
-            if not hasattr(self, 'pose'):
-                print msg.pose.pose
-            self.pose = msg.pose.pose
-        self.odom_sub = rospy.Subscriber('/odom', Odometry, getPose)
         self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.getScan)
         self.cmd_vel = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=10)
 
+        self.threads = []
+        th = threading.Thread(target=lambda:showImage(self))
+        th.start()
+        self.threads.append(th)
+
         # TurtleBot will stop if we don't keep telling it to move.
         # How often should we tell it to move? 10 HZ
-        r = rospy.Rate(10);
-        while not rospy.is_shutdown() and not self.reached(self.goalX, self.goalY):
+        r = rospy.Rate(10)
+        while not rospy.is_shutdown():
             # wait for 0.1 seconds (10 HZ) and publish again
             self.move()
             r.sleep()
 
     def getScan(self, msg):
-        if not hasattr(self, 'scanned'):
-            self.scanned = True
-            print(len(msg.ranges))
-
-    def reached(self, x, y):
-        if not hasattr(self, 'pose'):
-            return False
-        diffX = abs(self.pose.position.x-x)
-        diffY = abs(self.pose.position.y-y)
-        return (diffX < self.linearResolution) and (diffY < self.linearResolution)
+        self.scan = msg
 
     def move(self):
-        if not hasattr(self, 'pose'):
-            return
-        spin = self.calcSpin(self.pose.orientation, self.pose.position.x, self.pose.position.y, self.goalX, self.goalY)
+        spin = self.calcSpin()
+        vel = self.calcVel(spin)
+        self.pos.rot += spin
+        self.pos.x += np.cos(self.pos.rot)*vel
+        self.pos.y += np.sin(self.pos.rot)*vel
         move_cmd = Twist()
-        move_cmd.angular.z = spin * 0.6
-        move_cmd.linear.x = self.calcVel(spin)
+        move_cmd.angular.z = spin
+        move_cmd.linear.x = vel
         self.cmd_vel.publish(move_cmd)
 
     def calcVel(self, spin):
@@ -60,13 +91,8 @@ class Trab1():
             return (1 - abs(spin))*0.7
         return 0
 
-    def calcSpin(self, origOrient, origX, origY, destX, destY):
-        euler = tf.transformations.euler_from_quaternion((origOrient.x, origOrient.y, origOrient.z, origOrient.w))
-        curRads = euler[2]
-        x = destX - origX
-        y = destY - origY
-        destRads = normRad(np.arctan2(y,x))
-        return normRad(destRads - curRads)
+    def calcSpin(self):
+        return 1.0
 
     def shutdown(self):
         # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
@@ -76,6 +102,6 @@ class Trab1():
 
 if __name__ == '__main__':
     try:
-        Trab1(*sys.argv[1:])
+        Trab2(*sys.argv[1:])
     except rospy.exceptions.ROSInterruptException:
         pass
